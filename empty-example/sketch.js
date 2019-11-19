09c3072d29d86ba1bb6293c5802fa1ae4541879d
emili Guassu Gomes <emiliguassu291@gmail.com>
2019-11-19 16:26:55 -0200

    cobrinha inicializada

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,14 @@
+const tamanho = 10;
+
+
 function setup() {
   createCanvas(400,400);
+  cobrinha = new cobra ();
+  frameRate(2);
 }
 
 function draw() {
 background(51);
+cobrinha.show();
+cobrinha.update();
 }
